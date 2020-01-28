8763691e13242be212d25ff69599bc32b941fcbc
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-01-28 08:33:52 +0000

    Added 'height' to the 'RaisedButton' inside 'CustomRaisedButton' class. # Note: By default 'RaisedButton' doesn't have height property, so we have to wrap in widget that has that property i.e. 'SizedBox' # Wrapped 'RaisedButton' inside the 'CustomRaisedButton' class into a 'SizedBox' and provided 'height' property to it. # Also updated the 'Constructor' of 'CustomRaisedButton' to accept a new argument i.e. height. # Added a default value to the 'height' argument of the Constructor i.e. '50.0'

--- lib/custom_widgets/custom_raised_button.dart
+++ lib/custom_widgets/custom_raised_button.dart
@@ -4,26 +4,31 @@ class CustomRaisedButton extends StatelessWidget {
   final Widget child;
   final Color color;
   final double borderRadius;
+  final double height;
   final VoidCallback onPressed;
 
   CustomRaisedButton({
     this.child,
     this.color,
     this.borderRadius: 2.0,
+    this.height: 50.0,
     this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: child,
-      color: color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(borderRadius),
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        child: child,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
         ),
+        onPressed: onPressed,
       ),
-      onPressed: onPressed,
     );
   }
 }
