0f43ec11f9e109adb44e6829c661031f5ab32cc6
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-02-16 23:15:19 +0000

    Refactored to use a generic 'user' instead of 'FirebaseUser' # auth.dart >> Added a new 'User' class that only takes one argument i.e. uid # auth.dart >> Added a new method i.e. '_userFromFirebase' (inside 'Auth' class) that return a User from an Object of type 'FirebaseUser' # auth.dart >> Updated 'currentUser()' and 'signInAnonymously()' to return 'Future<User>' instead of 'Future<FirebaseUser>'

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class User {
+  User({@required this.uid});
+  final String uid;
+}
 
 class Auth {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  Future<FirebaseUser> currentUser() async {
-    return await _firebaseAuth.currentUser();
+  // Below method return a User from an Object of type 'FirebaseUser'
+  User _userFromFirebase(FirebaseUser user) {
+    if (user == null) {
+      return null;
+    }
+    return User(uid: user.uid);
   }
 
-  Future<FirebaseUser> signInAnonymously() async {
+  // Changing return type from 'FirebaseUser' to 'User'
+  Future<User> currentUser() async {
+    final user = await _firebaseAuth.currentUser();
+    return _userFromFirebase(user);
+  }
+
+  // Changing return type from 'FirebaseUser' to 'User'
+  Future<User> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
-    return authResult.user;
+    return _userFromFirebase(authResult.user);
   }
 
   Future<void> signOut() async {
