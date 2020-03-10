ba84bacac115e6c19966c5ce102b9965c90755d0
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-03-10 08:38:15 +0000

    StreamBuilder - refactoring 'landing_screen.dart' # landing_screen.dart >> As we are listening to the Stream and returning 'SignInScreen' or 'HomeScreen' depending on the stream value, therefore we don't have to use mutable user variable and the corresponding methods that uses that variable whenever the its value changes i.e. '_checkCurrentUser' and '_updateUser' # landing_screen.dart >> Removed mutable variable '_user' # landing_screen.dart >> Removed methods using mutable variable '_user' i.e. '_checkCurrentUser' and '_updateUser' # landing_screen.dart >> Removed 'initState' # landing_screen.dart >> Changed 'LandingScreen' from 'StatefulWidget' to 'StatelessWidget' # landing_screen.dart >> changed 'widget.auth' calls to 'auth' calls.

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -3,67 +3,25 @@ import 'package:time_tracker_flutter_course/screens/app_sign_in/sign_in_screen.d
 import 'package:time_tracker_flutter_course/screens/home_screen.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class LandingScreen extends StatefulWidget {
+class LandingScreen extends StatelessWidget {
   LandingScreen({@required this.auth});
   final AuthBase auth;
 
   @override
-  _LandingScreenState createState() => _LandingScreenState();
-}
-
-class _LandingScreenState extends State<LandingScreen> {
-  // '_user' will be used to check if a User is logged in or Not (i.e. if its value is NULL or not)
-  User _user;
-
-  // 'initState()' Will be called each time app is restarted and hence '_checkCurrentUser()' as well.
-  @override
-  void initState() {
-    super.initState();
-    _checkCurrentUser();
-    // Subscribing/Registering/Listening to the 'user' stream, produced by the 'onAuthStateChanged' getter method
-    widget.auth.onAuthStateChanged.listen((user) {
-      print('user:${user?.uid}');
-    });
-  }
-
-  // Function '_checkCurrentUser' will verify if User the is logged In or Not,
-  // and if User is logged in and App is restarted, User will remain logged In,
-  // as '_updateUser(user)' is called each time APP is restarted.
-  Future<void> _checkCurrentUser() async {
-    User user = await widget.auth.currentUser();
-    _updateUser(user);
-  }
-
-  // Reference of method will be passed to the (SignInScreen's) 'onSignIn' function as a Callback.
-  void _updateUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: widget.auth.onAuthStateChanged,
+      stream: auth.onAuthStateChanged,
       builder: (context, snapshot) {
         // Checking if Stream has returned at least one value, but is not yet done.
         if (snapshot.connectionState == ConnectionState.active) {
           User user = snapshot.data;
           if (user == null) {
-            return SignInScreen(
-              auth: widget.auth,
-              onSignIn: _updateUser,
-            );
+            return SignInScreen(auth: auth);
           }
-          return HomeScreen(
-            auth: widget.auth,
-            onSignOut: () => _updateUser(null),
-          );
+          return HomeScreen(auth: auth);
         } else {
           return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
+            body: Center(child: CircularProgressIndicator()),
           );
         }
       },
